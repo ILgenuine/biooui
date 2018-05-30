caa6fa89d156abb2a80214412ad40a915757b939
haritleksopee <33746030+haritleksopee@users.noreply.github.com>
2018-05-30 12:26:13 +0700

    add register.php 12.25 30/05/2561

--- login.php
+++ login.php
@@ -16,14 +16,55 @@
 
     <div class="wrapper">
       <div class="container">
-        <div class="row login-all-auto">
+
+
+        <div class="row py-5">
+          <div class="col-12 login-text-center-414">
+            <h1 class="login-font-head">
+              <i class="fa fa-sign-in" aria-hidden="true"></i> <font class="text-white">เข้าสู่ระบบ</font>
+            </h1>
+            <p class="text-white" style="letter-spacing: 1px;">กรอกชื่อผู้ใช้และรหัสผ่านเพื่อเข้าสู่ระบบ</p>
+          </div>
+        </div>
+
+
+        <div class="row">
+          <div class="col-12">
+            <form class="register-from-size">
+                <div class="form-group">
+                  <label class="text-white register-label">ชื่อผู้ใช้</label>
+                  <input type="text" class="form-control">
+                </div>
+                <div class="form-group">
+                  <label class="text-white register-label">รหัสผ่าน</label>
+                  <input type="password" class="form-control">
+                </div>
+
+                <div class="form-group">
+                  <div class="row login-mt-40px ">
+                    <a href="#" class="btn login-bnt" role="button">
+                      <font class="text-white">เข้าสู่ระบบ</font>
+                    </a> 
+                    <a href="#" class="btn login-bnt" role="button">
+                      <i class="fa fa-user" aria-hidden="true"></i> <font class="text-white">สมัครสมาชิก</font>
+                    </a>
+                    <a href="#" class="btn login-bnt" role="button">
+                      <i class="fa fa-key" aria-hidden="true"></i> <font class="text-white">ลืมรหัสผ่าน</font>
+                    </a>
+                  </div>
+                </div>
+            </form>
+          </div>
+        </div>
+
+<!--         <div class="row login-all-auto">
           <div class="col-xl-4 col-lg-4 col-12">
             
             <div class="login-line-text">
               <h1 class="login-font-head">
                 <i class="fa fa-sign-in" aria-hidden="true"></i> <font class="text-white">เข้าสู่ระบบ</font>
               </h1>
-              <font class="text-white">กรอกชื่อผู้ใช้และรหัสผ่านเพื่อเข้าสู่ระบบ</font>
+              <font class="text-white" style="letter-spacing: 1px;">กรอกชื่อผู้ใช้และรหัสผ่านเพื่อเข้าสู่ระบบ</font>
             </div>
 
 
@@ -55,7 +96,7 @@
 
           </div>
           <div class="col-xl-2 col-lg-1 col-12"></div>
-        </div>
+        </div> -->
       </div>
     </div>
 
